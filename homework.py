33f30616823ee125d088396e89d25f2c52edd737
Антон Почкай <anton.pochkai@mail.ru>
2022-04-28 16:29:08 +0300

    28.04 commit before rewuie check

--- homework.py
+++ homework.py
@@ -1,7 +1,8 @@
 from dataclasses import dataclass, asdict
-from typing import Union, Type
+from typing import Union
 
 import constants
+
 
 @dataclass
 class InfoMessage:
@@ -17,10 +18,12 @@ class InfoMessage:
         """Вывод сообщения о тренировке"""
         return self.info.format(**asdict(self))
 
+
 class Training:
     """Базовый класс тренировки."""
     LEN_STEP: float = constants.LEN_STEP
     M_IN_KM: int = constants.M_IN_KM
+
     def __init__(self,
                  action: int,
                  duration: float,
@@ -33,11 +36,11 @@ class Training:
     def get_distance(self) -> float:
         """Получить дистанцию в км."""
         return self.action * constants.LEN_STEP / constants.M_IN_KM
-    
+
     def get_mean_speed(self) -> float:
         """Получить среднюю скорость движения."""
         return self.get_distance() / self.duration
-        
+
     def get_spent_calories(self) -> float:
         """Получить количество затраченных калорий."""
     pass
@@ -51,6 +54,7 @@ class Training:
             self.get_mean_speed(),
             self.get_spent_calories())
 
+
 class Running(Training):
     """Тренировка: бег."""
     def __init__(self,
@@ -61,8 +65,11 @@ class Running(Training):
 
     def get_spent_calories(self) -> float:
         """Расчет потраченных калорий"""
-        return (constants.COEFF_CALORIE_run_1 * self.get_mean_speed() - constants.COEFF_CALORIE_run_2) * self.weight / constants.M_IN_KM * self.duration * constants.M_IN_H
-        
+        return (
+            constants.COEFF_CALORIE_run_1
+            * self.get_mean_speed()
+            - constants.COEFF_CALORIE_run_2) * self.weight / constants.M_IN_KM * (self.duration * constants.M_IN_H)
+
 
 class SportsWalking(Training):
     """Тренировка: спортивная ходьба."""
@@ -73,14 +80,23 @@ class SportsWalking(Training):
                  height: float) -> None:
         super().__init__(action, duration, weight)
         self.height = height
-            
+
     def get_spent_calories(self) -> float:
         """Рассчет потраченных калорий"""
-        return (constants.COEFF_CALORIE_wlk_1 * self.weight + (self.get_mean_speed() ** constants.COEFF_CALORIE_wlk_2 // self.height) * constants.COEFF_CALORIE_wlk_3 * self.weight * self.duration*constants.M_IN_H)
+        return (
+            constants.COEFF_CALORIE_wlk_1
+            * self.weight
+            + (self.get_mean_speed()
+            ** constants.COEFF_CALORIE_wlk_2
+            // self.height)
+            * constants.COEFF_CALORIE_wlk_3
+            * self.weight) * self.duration * constants.M_IN_H
+
 
 class Swimming(Training):
     """Тренировка: плавание."""
-    LEN_STEP:float = constants.LEN_STROKE
+    LEN_STEP: float = constants.LEN_STROKE
+
     def __init__(self,
                  action: int,
                  duration: float,
@@ -90,33 +106,39 @@ class Swimming(Training):
         super().__init__(action, duration, weight)
         self.length_pool = length_pool
         self.count_pool = count_pool
-    
+
     def get_distance(self) -> float:
         """Переопределения дистанции для плаванья"""
         return self.action * self.LEN_STEP / constants.M_IN_KM
-    
+
     def get_mean_speed(self) -> float:
         """Формула рассчета средней скорости при плаванье"""
-        return (self.length_pool * self.count_pool / constants.M_IN_KM / self.duration)
-        
+        return (self.length_pool
+                * self.count_pool
+                / constants.M_IN_KM
+                / self.duration)
+
     def get_spent_calories(self) -> float:
         """Рассчет потраченных калорий при плаванье"""
 
-        return ((self.get_mean_speed() + constants.COEFF_CALORIE_swm_1) 
-                * constants.COEFF_CALORIE_swm_2* self.weight)
+        return ((self.get_mean_speed() + constants.COEFF_CALORIE_swm_1)
+                * constants.COEFF_CALORIE_swm_2 * self.weight)
+
 
 def read_package(workout_type: str, data: list) -> Training:
     """Прочитать данные полученные от датчиков."""
     train_dict = {
-        "SWM" : Swimming,
+        "SWM": Swimming,
         "RUN": Running,
         "WLK": SportsWalking}
-    return train_dict[workout_type](*data)    
+    return train_dict[workout_type](*data)
+
 
 def main(training: Union[Training, Running, Swimming, SportsWalking]) -> None:
     """Главная функция."""
-    
+
     print(training.show_training_info().get_message())
+
 
 if __name__ == '__main__':
     packages = [
